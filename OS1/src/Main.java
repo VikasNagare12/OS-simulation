72745e2c6e3e4b97aba2bdf2304c11abbd3cfa92
VikasNagare45 <vikasnagare05@gmail.com>
2019-03-17 10:02:09 +0530

    Create Main.java

---
+++ OS1/src/Main.java
@@ -0,0 +1,24 @@
+import com.vikas.vm.VirtualMachine;
+/**
+ * 
+ */
+
+/**
+ * @author Vikas
+ *
+ */
+public class Main {
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		VirtualMachine vm=new VirtualMachine();
+		vm.load();
+	}
+}
