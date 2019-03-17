6a375a008c1927cc2e587841774323d2372f64b3
VikasNagare45 <vikasnagare05@gmail.com>
2019-03-17 19:24:38 +0530

    Remove duplicated directory

--- OS1/src/com/vikas/vm/CC.java
+++
@@ -1,7 +0,0 @@
-package com.vikas.vm;
-
-public enum CC {
-	$AMJ("$AMJ"),$DTA("$DTA"),$END("$END");
-	CC(String name) {	
-	}
-}
--- OS1/src/com/vikas/vm/PC.java
+++
@@ -1,5 +0,0 @@
-package com.vikas.vm;
-
-public enum PC {
-	GD,PD,H;
-}
